84adc3d8737c2f5f641cd4e92ee3dd2d53c6c340
Ibrohim <64252734+IbrohimSafarov@users.noreply.github.com>
2020-05-02 05:13:04 +0500

    Add files via upload

---
+++ figure/Plot3.R
@@ -0,0 +1,24 @@
+library("data.table")
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Subset the data
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
+voltage <- as.numeric(subsetdata$Voltage)
+subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+
+#Please note the subsetting data is identical as in Plot1.R
+
+#Create Plot3
+DateTime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+plot(DateTime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(DateTime, subMetering2, type="l", col="red")
+lines(DateTime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+## Saving to file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
